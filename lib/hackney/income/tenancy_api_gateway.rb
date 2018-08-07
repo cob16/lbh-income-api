7255372d19175ea7ebd9091099dd89e89fbbec75
Richard Foster <richard@rpf.me>
2018-08-07 04:14:29 +0100

    Add Tenancy API Gateway with list method

---
+++ lib/hackney/income/tenancy_api_gateway.rb
@@ -0,0 +1,42 @@
+module Hackney
+  module Income
+    class TenancyApiGateway
+      def initialize(host:)
+        @host = host
+      end
+
+      def get_tenancies_by_refs(refs)
+        return [] if refs.empty?
+
+        response = RestClient.get("#{@host}/api/v1/tenancies", params: { tenancy_refs: convert_to_params_array(refs) })
+        body = JSON.load(response.body)
+
+        body['tenancies'].map do |tenancy|
+          action_missing = tenancy.dig('latest_action', 'code').nil?
+          contact_missing = tenancy.dig('primary_contact', 'name').nil?
+
+          {
+            ref: tenancy.fetch('ref'),
+            current_balance: tenancy.fetch('current_balance'),
+            current_arrears_agreement_status: tenancy.fetch('current_arrears_agreement_status'),
+            latest_action: action_missing ? nil : {
+              code: tenancy.dig('latest_action', 'code'),
+              date: Time.parse(tenancy.dig('latest_action', 'date'))
+            },
+            primary_contact: contact_missing ? nil : {
+              name: tenancy.dig('primary_contact', 'name'),
+              short_address: tenancy.dig('primary_contact', 'short_address'),
+              postcode: tenancy.dig('primary_contact', 'postcode')
+            }
+          }
+        end
+      end
+
+      private
+
+      def convert_to_params_array(refs)
+        RestClient::ParamsArray.new(refs.map.with_index(0) { |e, i| [i.to_s, e] }.to_a)
+      end
+    end
+  end
+end
