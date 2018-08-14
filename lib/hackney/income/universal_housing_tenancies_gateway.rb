d6c9401860053297c5ea183dd83f92b6d5df6e8a
Steven Leighton <steven@madetech.com>
2018-08-14 13:48:50 +0100

    revert this, release to staging

--- lib/hackney/income/universal_housing_tenancies_gateway.rb
+++ lib/hackney/income/universal_housing_tenancies_gateway.rb
@@ -2,11 +2,7 @@ module Hackney
   module Income
     class UniversalHousingTenanciesGateway
       def tenancies_in_arrears
-        tenancy_refs = []
-        query = database.execute('SELECT tag_ref FROM tenagree WHERE cur_bal > 0')
-        query.each { |record| tenancy_refs << record.fetch('tag_ref').strip }
-        query.do
-        tenancy_refs
+        database[:tenagree].where { cur_bal > 0 }.map(:tag_ref).map(&:strip)
       end
 
       private
