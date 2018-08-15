6f33a1861f843667de20b9d225e26b8f2403fb93
Luke Morton <lukemorton.dev@gmail.com>
2018-08-15 11:45:40 +0100

    Add HardcodedTenanciesGateway
    This provides 30 cases selected by Elaine that will be good for testing.

---
+++ spec/lib/hackney/income/hardcoded_tenancies_gateway_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../../../../lib/hackney/income/hardcoded_tenancies_gateway'
+
+describe Hackney::Income::HardcodedTenanciesGateway do
+  let(:gateway) { described_class.new }
+
+  context 'retrieving tenancy refs for cases in arrears' do
+    subject { gateway.tenancies_in_arrears }
+
+    context 'when hardcoded tenancies set in ENV' do
+      before do
+        @hardcoded_tenancies = ENV['HARDCODED_TENANCIES']
+        ENV['HARDCODED_TENANCIES'] = '1,2,3'
+      end
+
+      after do
+        ENV['HARDCODED_TENANCIES'] = @hardcoded_tenancies
+      end
+
+      it 'should return 3 hardcoded tenancies' do
+        expect(subject.count).to eq(3)
+      end
+    end
+
+    context 'when hardcoded tenancies not set in ENV' do
+      it 'should raise error' do
+        expect{subject}.to raise_error(Hackney::Income::HardcodedTenanciesUndefinedError)
+      end
+    end
+  end
+end
