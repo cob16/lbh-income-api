e3207d155da83700f9bdf5287cbcad6ea919023a
Richard Foster <richard@rpf.me>
2018-08-28 17:20:50 +0100

    Make job scheduling reusable and ensure scheduled jobs are always scheduled

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,22 @@
 class ApplicationJob < ActiveJob::Base
+  class << self
+    def enqueue_next
+      unless already_queued_for_next_run?
+        set(wait_until: next_run_time).perform_later
+      end
+    end
+
+    def next_run_time
+      raise NotImplementedError
+    end
+
+    private
+
+    def already_queued_for_next_run?
+      Delayed::Job.any? do |next_job|
+        next_job_data = YAML.load(next_job.handler).job_data
+        name == next_job_data['job_class'] && next_run_time == next_job.run_at
+      end
+    end
+  end
 end
--- app/jobs/hackney/income/jobs/sync_cases_job.rb
+++ app/jobs/hackney/income/jobs/sync_cases_job.rb
@@ -4,15 +4,15 @@ module Hackney
       class SyncCasesJob < ApplicationJob
         queue_as :default
 
-        after_perform do
-          self.class.set(wait_until: next_run_time).perform_later
+        def self.next_run_time
+          Date.tomorrow.midnight + 3.hours
         end
 
         def perform
-          begin
-            sync_cases.execute
-          rescue
-          end
+          sync_cases.execute
+        rescue
+        ensure
+          self.class.enqueue_next
         end
 
         def sync_cases
@@ -21,10 +21,6 @@ module Hackney
             uh_tenancies_gateway: Hackney::Income::UniversalHousingTenanciesGateway.new,
             stored_tenancies_gateway: Hackney::Income::StoredTenanciesGateway.new
           )
-        end
-
-        def next_run_time
-          Date.tomorrow.midnight + 3.hours
         end
       end
     end
