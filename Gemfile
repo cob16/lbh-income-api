723f2944e8b894f0a06f5fb61a0abd560cee4f85
Richard Foster <richard@rpf.me>
2018-09-07 08:28:22 +0100

    Set up Sentry

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ end
 
 group :staging, :production do
   gem 'newrelic_rpm'
+  gem 'sentry-raven'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :set_raven_context
+
   def income_use_case_factory
     @income_use_case_factory ||= Hackney::Income::UseCaseFactory.new
+  end
+
+  def set_raven_context
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
