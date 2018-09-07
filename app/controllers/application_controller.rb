ad4102665d03d561192d19069d653d8076cafb3e
Richard Foster <richard@rpf.me>
2018-09-07 08:39:31 +0100

    Ignore sentry context in test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :set_raven_context
+  before_action :set_raven_context, if: :sentry_configured?
 
   def income_use_case_factory
     @income_use_case_factory ||= Hackney::Income::UseCaseFactory.new
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::API
 
   def set_raven_context
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+
+  def sentry_configured?
+    ENV.key?('SENTRY_DSN')
   end
 end
