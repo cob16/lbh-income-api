be896e065faed236ca221f9e5a31cf2a4a396f4b
Richard Foster <richard@rpf.me>
2018-09-10 14:50:35 +0100

    Update MyCases controller to read tenancy refs
    The ICS will pass in tenancy refs for cases assigned to the current user.

--- app/controllers/my_cases_controller.rb
+++ app/controllers/my_cases_controller.rb
@@ -1,25 +1,17 @@
 class MyCasesController < ApplicationController
   def index
-    cases = view_my_cases_use_case.execute(random_tenancy_refs)
+    cases = income_use_case_factory.view_my_cases.execute(tenancy_refs)
     render json: cases
   end
 
   def sync
-    sync_cases_use_case.execute
+    income_use_case_factory.sync_cases.execute
     render json: { success: true }
   end
 
   private
 
-  def view_my_cases_use_case
-    income_use_case_factory.view_my_cases
-  end
-
-  def sync_cases_use_case
-    income_use_case_factory.sync_cases
-  end
-
-  def random_tenancy_refs
-    Hackney::Income::Models::Tenancy.first(20).map { |t| t.tenancy_ref }
+  def tenancy_refs
+    params.fetch(:tenancy_refs, [])
   end
 end
