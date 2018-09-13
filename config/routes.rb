f442e2cd8bee586db7512a2cb7ce5a48a8fcf7a4
Steven Leighton <steven@madetech.com>
2018-09-13 16:17:55 +0100

    Check against missing params - behaviour could be improved

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/api/v1/my-cases', to: 'my_cases#index'
   get '/api/v1/sync-cases', to: 'my_cases#sync'
+  post '/api/v1/find-or-create-user', to: 'users#create'
 end
