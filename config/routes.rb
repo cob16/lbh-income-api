bd1779a86454c2d7748141e1ca9beeb3254c4aac
Steven Leighton <steven@madetech.com>
2018-10-11 15:43:39 +0100

    Group v1 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/api/v1/my-cases', to: 'my_cases#index'
-  get '/api/v1/sync-cases', to: 'my_cases#sync'
-  post '/api/v1/users/find-or-create', to: 'users#create'
-  patch '/api/v1/tenancies/set-pause-status', to: 'tenancies#update'
+  scope '/api/v1' do
+    get '/my-cases', to: 'my_cases#index'
+    get '/sync-cases', to: 'my_cases#sync'
+    post '/users/find-or-create', to: 'users#create'
+    patch '/tenancies/set-pause-status', to: 'tenancies#update'
+  end
 end
