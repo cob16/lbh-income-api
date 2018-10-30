b7deca6c1be716c10ecbc554946223b18da1af24
Mac <mac@bbtnetworks.com>
2018-10-30 15:13:53 +0000

    linting version fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,5 +98,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-  config.after(:suite) { Brakeman.run app_path: ".", summary: true }
+  config.after(:suite) { Brakeman.run app_path: '.', summary: true }
 end
