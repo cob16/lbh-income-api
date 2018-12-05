681525ee87060c106d8e77cf15d2432d701ff805
Mac <mac@bbtnetworks.com>
2018-12-05 10:01:18 +0000

    remove overwride

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ require 'action_cable/engine'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-ActiveSupport::Deprecation.behavior = :stderr
 
 module App
   class Application < Rails::Application
