193eb9e644c0661757a1624bdd3fbfcc2d557e4e
Mac <mac@bbtnetworks.com>
2018-12-07 12:07:08 +0000

    filter params

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters +=
+  [:password, :tenancy_ref, :email_address,
+   'first name', 'last name', 'full name']
