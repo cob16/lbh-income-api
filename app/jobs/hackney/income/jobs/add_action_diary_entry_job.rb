d630b7dfa4e2df018447ffcad20872f5f465b5d9
cob16 <public+github@cormacbrady.info>
2019-01-24 13:25:25 +0000

    stop action_diary_writer from retrying due to duplicate messages

--- app/jobs/hackney/income/jobs/add_action_diary_entry_job.rb
+++ app/jobs/hackney/income/jobs/add_action_diary_entry_job.rb
@@ -4,6 +4,10 @@ module Hackney
       class AddActionDiaryEntryJob < ApplicationJob
         queue_as :action_diary_writer
 
+        def max_attempts
+          1
+        end
+
         def perform(tenancy_ref:, action_code:, comment:)
           Rails.logger.info("Starting AddActionDiaryEntryJob for tenancy_ref #{tenancy_ref} and action code #{action_code}")
           income_use_case_factory.add_action_diary.execute(
