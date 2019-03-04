047a315997bc104655710fb8bba9bf556f49b6e4
Mac <mac@bbtnetworks.com>
2019-03-04 16:13:41 +0000

    remove redundant tests already covered in activejob

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,30 +6,4 @@ class ApplicationJob < ActiveJob::Base
   def run_tenancy_sync_jobs?
     Rails.configuration.x.run_tenancy_sync_jobs
   end
-
-  class << self
-    def enqueue_next
-      ActiveSupport::Deprecation.warn(
-        "enqueue_next is deprecated - use external scheduler via 'rake income:sync:enqueue'"
-      )
-
-      return if already_queued_for_next_run?
-
-      set(wait_until: next_run_time).perform_later
-    end
-
-    def next_run_time
-      raise NotImplementedError
-    end
-
-    private
-
-    def already_queued_for_next_run?
-      Delayed::Job.any? do |next_job|
-        whitelisted_classes = [ActiveJob::QueueAdapters::DelayedJobAdapter::JobWrapper]
-        next_job_data = YAML.safe_load(next_job.handler, whitelisted_classes, [], true).job_data
-        name == next_job_data['job_class'] && next_run_time == next_job.run_at
-      end
-    end
-  end
 end
