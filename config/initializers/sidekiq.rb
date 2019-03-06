9f2fbfe9eeb7e85baf33ddd6015f659c2a03a0cb
Mac <mac@bbtnetworks.com>
2019-03-06 13:49:20 +0000

    required seperation between config files and adds healthcheck

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
-redis_url = ENV.fetch('REDIS_URL')
+redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379')
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_url}" }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_url}" }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.default_worker_options = {
