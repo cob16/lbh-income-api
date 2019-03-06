2dc9a0c0834ff15a5e1b92598e14dae0052441cc
Mac <mac@bbtnetworks.com>
2019-03-06 16:32:59 +0000

    lookup path HF when rails is running

--- lib/schedules.rb
+++ lib/schedules.rb
@@ -1,4 +1,10 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
+rails_load_path = File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment')
+# load only ouside rails app
+if File.exists?(rails_load_path)
 
-require_relative 'schedules/hello_healthcheck.rb'
-require_relative 'schedules/tenancy_sync.rb'
+  require rails_load_path
+
+  require_relative 'schedules/hello_healthcheck.rb'
+  require_relative 'schedules/tenancy_sync.rb'
+
+end
