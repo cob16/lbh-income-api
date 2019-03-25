7aa88fd08e476db66b77934bca4efc3e145605e0
Mac <mac@bbtnetworks.com>
2019-03-25 16:26:12 +0000

    remove unneccessary class

--- spec/lib/hackney/cloud/adapter/aws_s3_spec.rb
+++ spec/lib/hackney/cloud/adapter/aws_s3_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
-# Aws::S3::Encryption::Client
-class FakeAwsS3Client < Aws::S3::Encryption::Client
-  def initialize(options = {})
-    binding.pry
-    super
-    @client = Aws::S3::Client.new(stub_responses: true)
-  end
-end
-
 describe Hackney::Cloud::Adapter::AwsS3 do
   let(:s3) { described_class.new Hackney::Cloud::EncryptionClient.new(ENV['CUSTOMER_MANAGED_KEY']).create }
   let(:filename) { 'test_key.pdf' }
@@ -17,7 +8,7 @@ describe Hackney::Cloud::Adapter::AwsS3 do
 
   let(:bucket_name) { 'hackney-docs-development' }
 
-  context 'upload' do
+  context 'when upload' do
     it 'is successful' do
       expect_any_instance_of(Aws::S3::Encryption::Client).to receive(:put_object).and_return(OpenStruct.new(successful?: true))
 
@@ -40,7 +31,7 @@ describe Hackney::Cloud::Adapter::AwsS3 do
     end
   end
 
-  context 'download' do
+  context 'when download' do
     it 'a file from S3' do
       expect_any_instance_of(Aws::S3::Encryption::Client).to receive(:get_object).and_return(OpenStruct.new(successful?: true))
 
