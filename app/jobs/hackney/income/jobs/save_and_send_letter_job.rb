086886fb90832bc0631e8cd789d18a316d6cbc94
Elena <elena@madetech.com>
2019-03-28 14:51:09 +0000

    adding crappy logging

--- app/jobs/hackney/income/jobs/save_and_send_letter_job.rb
+++ app/jobs/hackney/income/jobs/save_and_send_letter_job.rb
@@ -6,15 +6,24 @@ module Hackney
         queue_as :cloud_storage
 
         after_perform do |_job|
+          p '-_-_-_-_'
+          p 'start queueueueueueue'
+          p '-_-_-_-_'
           Rails.logger.info 'after_perform enqueuing send letter to gov notify'
-          Hackney::Income::Jobs::SendLetterToGovNotifyJob.perform_later
+          Hackney::Income::Jobs::SendLetterToGovNotifyJob.perform_later(document_id: @doc_id)
+          p '-_-_-_-_'
+          p 'end queueueueueueue'
+          p '-_-_-_-_'
         end
 
         def perform(bucket_name:, filename:, content:, document_id:)
           response = cloud_provider.upload(bucket_name: bucket_name,
                                            content: content,
                                            filename: filename)
-
+          p '-_-_-_-_'
+          pp response
+          p '-_-_-_-_'
+          @doc_id = document_id
           document(document_id).update!(url: response[:url], status: UPLOADED_CLOUD_STATUS)
         end
 
--- app/jobs/hackney/income/jobs/send_letter_to_gov_notify_job.rb
+++ app/jobs/hackney/income/jobs/send_letter_to_gov_notify_job.rb
@@ -5,17 +5,21 @@ module Hackney
         HACKNEY_BUCKET_DOCS = Rails.application.config_for('cloud_storage')['bucket_docs']
 
         def perform(document_id:)
+          p '----------'
+          p 'runninn send'
+          p '----------'
           document = Hackney::Cloud::Document.find(document_id)
 
           letter_pdf = pdf_file_from_s3(document.filename)
           metadata = get_metadata(document)
 
-          income_use_case_factory.send_precompiled_letter.execute(
+          thing = income_use_case_factory.send_precompiled_letter.execute(
             user_id: metadata[:user_id],
             payment_ref: metadata[:payment_ref],
             unique_reference: document.uuid,
             letter_pdf: letter_pdf
           )
+          pp thing
         end
 
         private
