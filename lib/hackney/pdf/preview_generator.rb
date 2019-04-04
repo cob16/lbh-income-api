842568ec2bdaf402a3660f0233ef940b670833d3
Miles-Alford <43789512+Miles-Alford@users.noreply.github.com>
2019-04-04 12:34:13 +0100

    Create reply form template & changes to all letter templates

--- lib/hackney/pdf/preview_generator.rb
+++ lib/hackney/pdf/preview_generator.rb
@@ -5,6 +5,7 @@ module Hackney
       LOGO_PATH = 'lib/hackney/pdf/templates/logo.svg'.freeze
       SENDER_ADDRESS_PATH = 'lib/hackney/pdf/templates/sender_address.erb'.freeze
       PAYMENT_OPTIONS_PATH = 'lib/hackney/pdf/templates/payment_options.erb'.freeze
+      REPLY_FORM_PATH = 'lib/hackney/pdf/templates/reply_form.erb'.freeze
 
       def initialize(template_path:)
         @template_path = template_path
@@ -19,6 +20,8 @@ module Hackney
         @sender_address = ERB.new(File.open(SENDER_ADDRESS_PATH).read).result(binding)
 
         @payment_options = ERB.new(File.open(PAYMENT_OPTIONS_PATH).read).result(binding)
+
+        @reply_form = ERB.new(File.open(REPLY_FORM_PATH).read).result(binding)
 
         template = File.open(@template_path).read
         html = ERB.new(template).result(binding)
