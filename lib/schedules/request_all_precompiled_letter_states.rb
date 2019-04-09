5d543ef04039ee4952329d04aca89a966ab7d7c8
Mac <mac@bbtnetworks.com>
2019-04-09 14:54:35 +0100

    HF add usecase factory

--- lib/schedules/request_all_precompiled_letter_states.rb
+++ lib/schedules/request_all_precompiled_letter_states.rb
@@ -6,4 +6,10 @@ class RequestAllPrecompiledLetterStates
     puts '* Scheduling RequestAllPrecompiledLetterState *'
     income_use_case_factory.enqueue_request_all_precompiled_letter_states.execute
   end
+
+  private
+
+  def income_use_case_factory
+    @income_use_case_factory ||= Hackney::Income::UseCaseFactory.new
+  end
 end
