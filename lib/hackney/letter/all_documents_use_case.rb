b20008cf42d6d298f2e8014337c4204f25043e90
Elena <elena@madetech.com>
2019-04-11 18:32:41 +0100

    adding sender username to metadata

--- lib/hackney/letter/all_documents_use_case.rb
+++ lib/hackney/letter/all_documents_use_case.rb
@@ -6,7 +6,13 @@ module Hackney
       end
 
       def execute
-        @cloud_storage.all_documents
+        @cloud_storage.all_documents.each do | doc |
+          metadata = JSON.parse(doc.metadata).deep_symbolize_keys
+          if metadata[:user_id]
+            metadata[:user_name] = Hackney::Income::Models::User.find(metadata[:user_id]).name
+            doc.metadata = metadata.to_json
+          end
+        end
       end
     end
   end
