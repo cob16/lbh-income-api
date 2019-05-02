e36f0f0ff46dab09a11a5c53852298fd0d11351e
Mac <mac@bbtnetworks.com>
2019-05-02 12:00:37 +0100

    log message updating

--- lib/hackney/notification/request_precompiled_letter_state.rb
+++ lib/hackney/notification/request_precompiled_letter_state.rb
@@ -14,11 +14,11 @@ module Hackney
       def update_document(message_id:, status:)
         store = Hackney::Cloud::Document
         doc = store.find_by!(ext_message_id: message_id)
+        Rails.logger.info "Document ext_message_id #{message_id} found with status #{status}"
         doc.status = status
         doc.save!
 
         Raven.send_event("Document has failed - id: #{doc.id}, uuid: #{doc.uuid}") if doc.failed?
-
       end
     end
   end
