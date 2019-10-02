8ddbac725da6c502ecd89a1a22b7868f38613b1a
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-10-02 10:34:31 +0100

    Add rake task to backfill letter records (#198)
    This commit adds a rake task that I poorly understand, but would prefer to have
     available rather than sitting in a branch.

--- app/models/hackney/cloud/document.rb
+++ app/models/hackney/cloud/document.rb
@@ -1,6 +1,9 @@
 module Hackney
   module Cloud
     class Document < ApplicationRecord
+      # the end status maps to https://docs.notifications.service.gov.uk/java.html#status-letter
+      # Accepted    GOV.UK Notify has sent the letter to the provider to be printed.
+      # Received    The provider has printed and dispatched the letter.
       enum status: { uploading: 0, uploaded: 1, received: 2, accepted: 3, 'validation-failed' => 4 }
 
       def failed?
