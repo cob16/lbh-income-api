430fe041515db9e5a46414efb3758cb0d986ee71
Maysa <maysa@madetech.com>
2019-10-08 16:49:32 +0100

    extract method

--- lib/hackney/income/tenancy_prioritiser/tenancy_classification.rb
+++ lib/hackney/income/tenancy_prioritiser/tenancy_classification.rb
@@ -19,22 +19,19 @@ module Hackney
 
         def send_sms?
           @criteria.last_communication_action.nil? &&
-          @criteria.balance >= 5 &&
-          @criteria.balance < 10 &&
-          @criteria.nosp_served == false &&
-          @criteria.last_communication_date <= 7.days.ago.to_date &&
-          @criteria.last_communication_date >= 3.months.ago.to_date &&
-          @criteria.paused == false
-
+            @criteria.balance >= 5 &&
+            @criteria.balance < 10 &&
+            @criteria.nosp_served == false &&
+            last_communication_between_three_months_one_week &&
+            @criteria.paused == false
         end
 
         def send_letter_one?
           @criteria.last_communication_action == Hackney::Tenancy::ActionCodes::TEXT_MESSAGE_SENT &&
             @criteria.balance > 10 &&
             @criteria.nosp_served == false &&
-            @criteria.last_communication_date <= 7.days.ago.to_date &&
-            @criteria.last_communication_date >= 3.months.ago.to_date &&
-          @criteria.paused == false
+            last_communication_between_three_months_one_week &&
+            @criteria.paused == false
         end
 
         def send_letter_two?
@@ -42,30 +39,29 @@ module Hackney
             @criteria.balance >= @criteria.weekly_rent &&
             @criteria.balance < (@criteria.weekly_rent * 3) &&
             @criteria.nosp_served == false &&
-            @criteria.last_communication_date <= 7.days.ago.to_date &&
-            @criteria.last_communication_date >= 3.months.ago.to_date &&
-          @criteria.paused == false
-
+            last_communication_between_three_months_one_week &&
+            @criteria.paused == false
         end
 
         def send_warning_letter?
           @criteria.last_communication_action == Hackney::Tenancy::ActionCodes::LETTER_2_IN_ARREARS_LH &&
-          @criteria.balance >= (@criteria.weekly_rent * 3) &&
-          @criteria.nosp_served == false &&
-          @criteria.last_communication_date <= 7.days.ago.to_date &&
-          @criteria.last_communication_date >= 3.months.ago.to_date &&
-          @criteria.paused == false
-
+            @criteria.balance >= (@criteria.weekly_rent * 3) &&
+            @criteria.nosp_served == false &&
+            last_communication_between_three_months_one_week &&
+            @criteria.paused == false
         end
 
         def send_nosp?
           @criteria.last_communication_action == 'ZW2' &&
-          @criteria.balance >= (@criteria.weekly_rent * 4) &&
-          @criteria.nosp_served == false &&
-          @criteria.last_communication_date <= 7.days.ago.to_date &&
-          @criteria.last_communication_date >= 3.months.ago.to_date &&
-          @criteria.paused == false
+            @criteria.balance >= (@criteria.weekly_rent * 4) &&
+            @criteria.nosp_served == false &&
+            last_communication_between_three_months_one_week &&
+            @criteria.paused == false
+        end
 
+        def last_communication_between_three_months_one_week
+          @criteria.last_communication_date <= 7.days.ago.to_date &&
+            @criteria.last_communication_date >= 3.months.ago.to_date
         end
       end
     end
