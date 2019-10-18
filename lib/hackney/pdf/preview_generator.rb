b920ae67b14a9ed16487c5e2bebe604ca4759fba
Miles Alford <malford@MacBook-Pro-2.local>
2019-10-18 15:25:20 +0100

    add LBA return date variable for LBA template

--- lib/hackney/pdf/preview_generator.rb
+++ lib/hackney/pdf/preview_generator.rb
@@ -10,11 +10,12 @@ module Hackney
       REPLY_FORM_LBA_PATH = 'lib/hackney/pdf/templates/layouts/reply_form_lba.erb'.freeze
       FINANCIAL_STATEMENT_LBA_PATH = 'lib/hackney/pdf/templates/layouts/financial_statement_lba.erb'.freeze
       PAYMENT_TABLES_LBA_PATH = 'lib/hackney/pdf/templates/layouts/payment_tables_lba.erb'.freeze
-
+  
       def initialize(template_path:)
         @template_path = template_path
         @errors = []
         @sending_date = get_date
+        @return_date_lba = get_return_date
         @logo = File.open(LOGO_PATH).read
       end
 
@@ -50,6 +51,10 @@ module Hackney
 
       def get_date
         (Time.now + 1.day).strftime('%d %B %Y')
+      end
+
+      def get_return_date
+      (Time.now + 31.days).strftime('%d %B %Y')
       end
     end
   end
