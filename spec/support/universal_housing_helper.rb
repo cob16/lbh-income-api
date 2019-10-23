0b51cebaa3e4eab42d5c118a4d0c9f07d6b0e4dd
George Schena <george@madetech.com>
2019-10-23 12:02:03 +0100

    Disable rubocop module length line limit

--- spec/support/universal_housing_helper.rb
+++ spec/support/universal_housing_helper.rb
@@ -3,8 +3,7 @@ module UniversalHousingHelper
   def create_uh_tenancy_agreement(tenancy_ref:, current_balance: 0.0, rent: 5.0, prop_ref: '', terminated: false, cot: '',
                                   tenure_type: 'SEC', high_action: '111', u_saff_rentacc: '', house_ref: '',
                                   nosp_notice_served_date: '1900-01-01 00:00:00 +0000', nosp_notice_expiry_date: '1900-01-01 00:00:00 +0000',
-                                  money_judgement: 0.0, charging_order: 0.0, bal_dispute: 0.0,
-                                  courtdate: '1900-01-01 00:00:00 +0000', court_outcome: nil)
+                                  money_judgement: 0.0, charging_order: 0.0, bal_dispute: 0.0, courtdate: '1900-01-01 00:00:00 +0000', court_outcome: nil)
     Hackney::UniversalHousing::Client.connection[:tenagree].insert(
       tag_ref: tenancy_ref,
       cur_bal: current_balance,
