d51a27c463624329b12c3363a162bc8ffb3c89fd
Richard Race <richard.race@madetech.com>
2019-11-04 12:00:09 +0000

    Do not use User Model to get user's name

--- lib/hackney/letter/all_documents_use_case.rb
+++ lib/hackney/letter/all_documents_use_case.rb
@@ -8,20 +8,12 @@ module Hackney
       def execute(payment_ref: nil)
         @cloud_storage.all_documents(payment_ref: payment_ref)
                       .each do |doc|
-          next unless doc.metadata
-          metadata = JSON.parse(doc.metadata).deep_symbolize_keys
-          if metadata[:user_id]
-            metadata[:user_name] = user_name(metadata[:user_id])
-            doc.metadata = metadata.to_json
-          end
+          metadata = JSON.parse(doc.metadata).deep_symbolize_keys if doc.metadata
+          metadata ||= {}
+          metadata[:username] = doc.username
+
+          doc.metadata = metadata.to_json
         end
-      end
-
-      private
-
-      def user_name(user_id)
-        user = Hackney::Income::Models::User.find_by(id: user_id)
-        user ? user.name : nil
       end
     end
   end
