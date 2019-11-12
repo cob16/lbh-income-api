207e4077106f84dd1f19b9358dea705b27cca50b
Elena <elena@madetech.com>
2019-11-12 16:48:56 +0000

    get templates usecase

--- lib/hackney/pdf/get_templates.rb
+++ lib/hackney/pdf/get_templates.rb
@@ -1,12 +1,13 @@
 module Hackney
   module PDF
     class GetTemplates
-      def initialize(template_directory_path:)
-        @template_directory = template_directory_path
-      end
+      LEASEHOLD_SERVICES_TEMPLATE_DIRECTORY_PATH = 'lib/hackney/pdf/templates/leasehold'.freeze
+      INCOME_COLLECTION_TEMPLATE_DIRECTORY_PATH = 'lib/hackney/pdf/templates/income'.freeze
+      LEASEHOLD_SERVICES_GROUP = 'leasehold-services-group-1'.freeze
+      INCOME_COLLECTION_GROUP = 'income-collection-group-1'.freeze
 
-      def execute
-        Dir.glob("#{@template_directory}*.erb").map do |f|
+      def execute(user_groups:)
+        Dir.glob("#{get_template_directory_path(user_groups)}*.erb").map do |f|
           template_meta_data = get_meta_data(f)
           { path: f, name: template_meta_data[:name], id: template_meta_data[:id] }
         end
@@ -27,6 +28,12 @@ module Hackney
 
       def get_template_id(file_path)
         File.basename(file_path, '.*')
+      end
+
+      # TODO need to figure out what to do when a user belongs in both....
+      def get_template_directory_path(groups)
+        return LEASEHOLD_SERVICES_TEMPLATE_DIRECTORY_PATH if groups.include?(LEASEHOLD_SERVICES_GROUP)
+        return INCOME_COLLECTION_TEMPLATE_DIRECTORY_PATH if groups.include?(INCOME_COLLECTION_GROUP)
       end
     end
   end
--- spec/lib/hackney/pdf/get_templates_spec.rb
+++ spec/lib/hackney/pdf/get_templates_spec.rb
@@ -2,14 +2,36 @@ require 'rails_helper'
 
 describe Hackney::PDF::GetTemplates do
   subject do
-    described_class.new(
-      template_directory_path: 'spec/lib/hackney/pdf/'
-    )
+    described_class.new()
   end
 
-  it 'finds the templates are found' do
-    expect(subject.execute).to eq([{
-      id: 'test_template', name: 'Test template', path: 'spec/lib/hackney/pdf/test_template.erb'
-    }])
+  let(:test_template_path) { 'spec/lib/hackney/pdf/test_template.erb' }
+
+  context 'finding leasehold templates' do
+
+    it 'templates are found in the correct directory' do
+      allow(Dir).to receive(:glob)
+                      .with("#{Hackney::PDF::GetTemplates::LEASEHOLD_SERVICES_TEMPLATE_DIRECTORY_PATH}*.erb")
+                      .and_return([test_template_path])
+
+      expect(subject.execute(user_groups: [Hackney::PDF::GetTemplates::LEASEHOLD_SERVICES_GROUP]))
+        .to eq([{
+                  id: 'test_template', name: 'Test template', path: test_template_path
+                }])
+    end
+  end
+
+  context 'finding income templates' do
+
+    it 'templates are found in the correct directory' do
+      allow(Dir).to receive(:glob)
+                      .with("#{Hackney::PDF::GetTemplates::INCOME_COLLECTION_TEMPLATE_DIRECTORY_PATH}*.erb")
+                      .and_return([test_template_path])
+
+      expect(subject.execute(user_groups: [Hackney::PDF::GetTemplates::INCOME_COLLECTION_GROUP]))
+        .to eq([{
+                  id: 'test_template', name: 'Test template', path: test_template_path
+                }])
+    end
   end
 end
