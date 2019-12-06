39c58b549b1bb292c2250dab1c0950dc383af3a1
George Schena <40758489+georgeschena@users.noreply.github.com>
2019-12-06 11:59:59 +0000

    Allow patch code env list to take whitespaces (#273)

--- spec/use_cases/case_ready_for_automation_spec.rb
+++ spec/use_cases/case_ready_for_automation_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe UseCases::CaseReadyForAutomation do
   let(:use_case) { described_class.new }
 
-  let(:patch_code_1) { 'ABC' }
-  let(:patch_code_not_in_automation_list) { 'BAA' }
+  let(:patch_code_1) { 'W02' }
+  let(:patch_code_not_in_automation_list) { 'W03' }
 
   before do
     allow(use_case).to receive(:patch_codes_allowed_for_automation_env).and_return(patch_codes_env)
@@ -27,7 +27,7 @@ describe UseCases::CaseReadyForAutomation do
   end
 
   context 'when the patch code automation env is defined' do
-    let(:patch_codes_env) { 'ABC,DEF,GHI' }
+    let(:patch_codes_env) { 'W02,W04,W06' }
 
     it 'returns true if the patch code exists in the env' do
       expect(use_case.execute(patch_code: patch_code_1)).to eq(true)
@@ -35,6 +35,14 @@ describe UseCases::CaseReadyForAutomation do
 
     it 'returns false if the patch code does not exist in the env' do
       expect(use_case.execute(patch_code: patch_code_not_in_automation_list)).to eq(false)
+    end
+  end
+
+  context 'when the patch code env varibale has spacing' do
+    let(:patch_codes_env) { 'W02, W04, W06' }
+
+    it 'returns true if the patch code exists in the env' do
+      expect(use_case.execute(patch_code: patch_code_1)).to eq(true)
     end
   end
 end
