e8c16931e1cbbdfadd0223e3c1784ff4966da461
Elena <elena@madetech.com>
2019-12-17 16:02:34 +0000

    appending empty lines to income collection letters so they pass gov notify validation

--- app/models/hackney/income_collection/letter.rb
+++ app/models/hackney/income_collection/letter.rb
@@ -8,7 +8,7 @@ module Hackney
       attr_reader :tenancy_ref, :address_line1, :address_line2,
                   :address_line3, :address_line4, :address_post_code,
                   :payment_ref, :total_collectable_arrears_balance,
-                  :title, :forename, :surname, :errors
+                  :title, :forename, :surname, :errors, :tenant_address
 
       # Following the pattern used in Letter.rb
       # We don't need template_path at the moment.
@@ -24,15 +24,17 @@ module Hackney
 
         @tenancy_ref = validated_params[:tenancy_ref]
         @payment_ref = validated_params[:payment_ref]
-        @address_line1 = validated_params[:address_line1]
-        @address_line2 = validated_params[:address_line2]
-        @address_line3 = validated_params[:address_line3]
-        @address_line4 = validated_params[:address_line4]
-        @address_post_code = validated_params[:address_post_code]
         @total_collectable_arrears_balance = format('%.2f', (validated_params[:total_collectable_arrears_balance] || 0))
         @title = validated_params[:title]
         @forename = validated_params[:forename]
         @surname = validated_params[:surname]
+        @tenant_address = build_tenant_address([
+          validated_params[:address_line1],
+          validated_params[:address_line2],
+          validated_params[:address_line3],
+          validated_params[:address_line4],
+          validated_params[:address_post_code]
+        ])
       end
 
       def validate_mandatory_fields(mandatory_fields, letter_params)
@@ -44,6 +46,21 @@ module Hackney
         )
 
         letter_params
+      end
+
+      private
+
+      def build_tenant_address(address_lines)
+        address_lines = address_lines.select(&:present?)
+
+        # Our templates are designed to have 5 lines in the address window. If the
+        # tenant address doesn't have full 5 lines, we need to add enough breaks
+        # to make up for that otherwise the whole letter won't pass gov notify validation
+        (5 - address_lines.length).times do
+          address_lines.push('<br>')
+        end
+
+        address_lines.join('<br>')
       end
     end
   end
