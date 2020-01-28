76cfd5efcaa86ca228b173023d8ef5fd3639678a
Richard Race <richard.race@madetech.com>
2020-01-28 12:18:13 +0000

    Feature flag for enabling automation of SMS

--- lib/hackney/income/schedule_send_sms.rb
+++ lib/hackney/income/schedule_send_sms.rb
@@ -7,11 +7,19 @@ module Hackney
       end
 
       def execute
+        return unless env_allowed_to_send_automated_sms?
+
         tenancies_list = @matching_criteria_gateway.send_sms_messages
         Rails.logger.info("About to schedule #{tenancies_list.length} green in arrears msg jobs")
         tenancies_list.each do |tenancy|
           @background_job_gateway.schedule_send_sms_msg(case_id: tenancy.case_id)
         end
+      end
+
+      private
+
+      def env_allowed_to_send_automated_sms?
+        App::Application.feature_toggle('AUTOMATE_INCOME_COLLECTION_SMS')
       end
     end
   end
--- spec/lib/hackney/income/schedule_send_sms_spec.rb
+++ spec/lib/hackney/income/schedule_send_sms_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
 describe Hackney::Income::ScheduleSendSMS do
-  subject { sync_cases.execute }
+  subject { schedule_send_sms.execute }
 
   let(:matching_criteria_gateway) { double(Hackney::Income::SqlTenanciesMatchingCriteriaGateway) }
   let(:background_job_gateway) { double(Hackney::Income::BackgroundJobGateway) }
 
-  let(:sync_cases) do
+  let(:schedule_send_sms) do
     described_class.new(
       matching_criteria_gateway: matching_criteria_gateway, background_job_gateway: background_job_gateway
     )
   end
 
-  context 'when syncing cases' do
+  context 'when allowed to send SMS' do
+    before do
+      expect(schedule_send_sms).to receive(:env_allowed_to_send_automated_sms?).and_return(true)
+    end
+
     context 'with no cases found' do
       before do
         expect(matching_criteria_gateway).to receive(:send_sms_messages).and_return([]).once
       end
 
       it 'queues no jobs' do
-        expect(background_job_gateway).not_to receive(:send_sms_messages)
+        expect(background_job_gateway).not_to receive(:schedule_send_sms_msg)
         subject
       end
     end
@@ -35,6 +39,36 @@ describe Hackney::Income::ScheduleSendSMS do
       it 'queues a job to sync that case' do
         expect(background_job_gateway).to receive(:schedule_send_sms_msg).with(case_id: case_priority_1.case_id).once
         expect(background_job_gateway).to receive(:schedule_send_sms_msg).with(case_id: case_priority_2.case_id).once
+        subject
+      end
+    end
+  end
+
+
+  context 'when not allowed to send SMS' do
+    context 'with no cases found' do
+      before do
+        expect(matching_criteria_gateway).not_to receive(:send_sms_messages)
+      end
+
+      it 'queues no jobs' do
+        expect(background_job_gateway).not_to receive(:schedule_send_sms_msg)
+
+        subject
+      end
+    end
+
+    context 'with cases found' do
+      let(:case_priority_1) { create(:case_priority) }
+      let(:case_priority_2) { create(:case_priority) }
+
+      before do
+        expect(matching_criteria_gateway).not_to receive(:send_sms_messages)
+      end
+
+      it 'queues a job to sync that case' do
+        expect(background_job_gateway).not_to receive(:schedule_send_sms_msg)
+
         subject
       end
     end
